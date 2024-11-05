eb452c3ec70cf1ab311af67c95d63f9ed2bbabfc
iremozacar <iremozacaar@gmail.com>
2024-11-05 14:16:16 +0300

    .

--- Backend/ArgentoApp.Backend.Entity/obj/Debug/net8.0/ArgentoApp.Backend.Entity.AssemblyInfo.cs
+++ Backend/ArgentoApp.Backend.Entity/obj/Debug/net8.0/ArgentoApp.Backend.Entity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArgentoApp.Backend.Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0a09aa09e3d43badd83dc362ad17a880848ae03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+354f7c7ae85b4f48899a3d8ec8e8c0955df41da8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArgentoApp.Backend.Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArgentoApp.Backend.Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
