1565c2484695a06992b9a06a67a336b0b73499db
iremozacar <iremozacaar@gmail.com>
2024-11-07 17:10:40 +0300

    .

--- Frontend/ArgentoApp.Frontend.Mvc/Repositories/ProductRepository.cs
+++ Frontend/ArgentoApp.Frontend.Mvc/Repositories/ProductRepository.cs
@@ -7,25 +7,18 @@ namespace ArgentoApp.Frontend.Mvc.Repositories;
 
 public static class ProductRepository
 {
-public static async List<ProductModel> GetHomesAsync (bool isHome=true){
+public static async Task<List<ProductModel>> GetHomesAsync (bool isHome=true){
 
-        ResponseModel<List<ProductModel>> responseCategoryModel = new();
+        ResponseModel<List<ProductModel>> responseProductModel = new();
         using (HttpClient httpClient = new HttpClient())
         {
             HttpResponseMessage httpResponseMessage = await httpClient.GetAsync($"http://localhost:5000/api/Categories/GetHomes/{isHome}");
             string contentResponse = await httpResponseMessage.Content.ReadAsStringAsync();
-            responseCategoryModel = JsonConvert.DeserializeObject<ResponseModel<List<ProductModel>>>(contentResponse);
+            responseProductModel = JsonConvert.DeserializeObject<ResponseModel<List<ProductModel>>>(contentResponse);
 
         }
         List<ProductModel> responseProductList = responseProductModel.IsSucceeded ? responseProductModel.Data : [];
 
-
-
-        ProductsCategories model = new()
-        {
-            CategoryList = responseCategoryList,
-            ProductList = null
-        };
 
         return responseProductList;
     }
