f11c2e4cc28507d96496f7151943c58ea7689ee8
iremozacar <iremozacaar@gmail.com>
2024-11-21 22:20:10 +0300

    .

--- Backend/ArgentoApp.Backend.Data/obj/Debug/net8.0/ArgentoApp.Backend.Data.AssemblyInfo.cs
+++ Backend/ArgentoApp.Backend.Data/obj/Debug/net8.0/ArgentoApp.Backend.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArgentoApp.Backend.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87ac65ed02ec0b050acad10f2a29d09ad3de3943")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+507795ef3d031b2a47f1f2b8e8f3eb89a1984292")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArgentoApp.Backend.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArgentoApp.Backend.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Backend/ArgentoApp.Backend.Entity/obj/Debug/net8.0/ArgentoApp.Backend.Entity.AssemblyInfo.cs
+++ Backend/ArgentoApp.Backend.Entity/obj/Debug/net8.0/ArgentoApp.Backend.Entity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArgentoApp.Backend.Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d2f850ce8345f54a4a3ce043d8cbff3b8199c81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+507795ef3d031b2a47f1f2b8e8f3eb89a1984292")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArgentoApp.Backend.Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArgentoApp.Backend.Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
