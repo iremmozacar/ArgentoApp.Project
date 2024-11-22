0dba6bef72fcdc6b5d39c8b98d3234a7c0b0a243
iremozacar <iremozacaar@gmail.com>
2024-11-22 17:50:40 +0300

    .

--- Backend/ArgentoApp.Backend.Shared/obj/Debug/net8.0/ArgentoApp.Backend.Shared.AssemblyInfo.cs
+++ Backend/ArgentoApp.Backend.Shared/obj/Debug/net8.0/ArgentoApp.Backend.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArgentoApp.Backend.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f11c2e4cc28507d96496f7151943c58ea7689ee8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c5de5cc3d861d72e5037375f92abb8509141719")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArgentoApp.Backend.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArgentoApp.Backend.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
